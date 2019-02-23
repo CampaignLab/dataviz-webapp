cf4d66e48d35a6392aca0b79675ddaeeb7ab967b
Bob Whitelock <bob.whitelock1@gmail.com>
2019-02-23 15:44:16 +0000

    Draw chart with every possible x
    Very slow and messy atm.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
 import './App.css';
-import Plot from 'react-plotly.js';
 import {csv} from 'd3-fetch';
-import * as d3 from 'd3';
-import * as ss from 'simple-statistics';
+
+import Chart from './Chart';
 
 const dataFile = 'health_data_merged_with_election_results.csv';
 
@@ -21,52 +20,17 @@ class App extends Component {
       return <div />;
     }
 
-    const get_clean_data = (data, variable_x, variable_y) =>
-      data.map((d) => [+d[variable_x], +d[variable_y]]);
-
-    const get_linear_regression = (data, variable_x, variable_y) =>
-      ss.linearRegressionLine(
-        ss.linearRegression(get_clean_data(data, variable_x, variable_y)),
-      );
-
-    const get_rsquared = (data, variable_x, variable_y) => {
-      let data_to_fit = get_clean_data(data, variable_x, variable_y);
-      let model = get_linear_regression(data, variable_x, variable_y);
-      return ss.rSquared(data_to_fit, model);
-    };
-
     const {data} = this.state;
 
     const possibleXs = Object.keys(data[0]).filter(
-      (d) => ['Area Code', 'Area Name', 'Parent Name'].indexOf(d) == -1,
+      (d) => ['Area Code', 'Area Name', 'Parent Name'].indexOf(d) === -1,
     );
-
-    const x = possibleXs[0];
-    const y = 'delta_Lab';
-
-    let minmax = [
-      d3.min(get_clean_data(data, x, y).map((d) => d[0])),
-      d3.max(get_clean_data(data, x, y).map((d) => d[0])),
-    ];
-
-    const plotData = [
-      {
-        x: data.map((d) => +d[x]),
-        y: data.map((d) => +d[y]),
-        mode: 'markers',
-        type: 'scatter',
-      },
-      {
-        x: minmax,
-        y: minmax.map((d) => get_linear_regression(data, x, y)(d)),
-        mode: 'lines',
-        type: 'scatter',
-      },
-    ];
 
     return (
       <div className="app">
-        <Plot data={plotData} layout={{width: 1000, height: 1000, title: x}} />
+        {possibleXs.map((x) => (
+          <Chart data={data} x={x} key={x} />
+        ))}
       </div>
     );
   }
---
+++ src/Chart.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import Plot from 'react-plotly.js';
+import * as d3 from 'd3';
+import * as ss from 'simple-statistics';
+
+const get_clean_data = (data, variable_x, variable_y) =>
+  data.map((d) => [+d[variable_x], +d[variable_y]]);
+
+const get_linear_regression = (data, variable_x, variable_y) =>
+  ss.linearRegressionLine(
+    ss.linearRegression(get_clean_data(data, variable_x, variable_y)),
+  );
+
+const get_rsquared = (data, variable_x, variable_y) => {
+  let data_to_fit = get_clean_data(data, variable_x, variable_y);
+  let model = get_linear_regression(data, variable_x, variable_y);
+  return ss.rSquared(data_to_fit, model);
+};
+
+export default class Chart extends Component {
+  render() {
+    const {data, x} = this.props;
+
+    const y = 'delta_Lab';
+
+    let minmax = [
+      d3.min(get_clean_data(data, x, y).map((d) => d[0])),
+      d3.max(get_clean_data(data, x, y).map((d) => d[0])),
+    ];
+
+    const plotData = [
+      {
+        x: data.map((d) => +d[x]),
+        y: data.map((d) => +d[y]),
+        mode: 'markers',
+        type: 'scatter',
+      },
+      {
+        x: minmax,
+        y: minmax.map((d) => get_linear_regression(data, x, y)(d)),
+        mode: 'lines',
+        type: 'scatter',
+      },
+    ];
+
+    return (
+      <Plot data={plotData} layout={{width: 1000, height: 1000, title: x}} />
+    );
+  }
+}
