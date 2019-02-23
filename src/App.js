e6e77aa6f5e4519ac433cfed669ff540240a4fb7
Bob Whitelock <bob.whitelock1@gmail.com>
2019-02-23 14:15:07 +0000

    Handle initial data loading with D3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
 import './App.css';
 import Plot from 'react-plotly.js';
+import {csv} from 'd3-fetch';
+// import data from 'data//health_data_merged_with_election_results.csv';
+
+const dataFile = 'health_data_merged_with_election_results.csv';
 
 class App extends Component {
+  componentWillMount() {
+    csv(dataFile).then((data) =>
+      this.setState({
+        data,
+      }),
+    );
+  }
+
   render() {
+    console.log(this.state);
+
     return (
       <div className="app">
         <Plot
